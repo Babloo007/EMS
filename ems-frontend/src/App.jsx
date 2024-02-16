9342daa6615688369dc565e7ad1fe02f55e5761a
Babloo007 <msvreddy77@gmail.com>
2024-02-16 13:43:41 +0530

    footer issue fixed

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -12,24 +12,29 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          {/* // http://localhost:3000 */}
-          <Route path="/" element={<EmployeeList />}></Route>
-          {/* // http://localhost:3000/employees */}
-          <Route path='/employees' element = { <EmployeeList /> }></Route>
-          {/* // http://localhost:3000/add-employee */}
-          <Route path='/add-employee' element = { <Employee />}></Route>
-          {/* // http://localhost:3000/edit-employee/1 */}
-          <Route path='/edit-employee/:id' element = { <Employee /> }></Route>
-          {/* // http://localhost:3000/departments */}
-          <Route path='/departments' element = { <DepartmentList /> }></Route>
-          {/* // http://localhost:3000/add-department */}
-          <Route path='/add-department' element = { <Department />}></Route>
-          {/* // http://localhost:3000/edit-department */}
-          <Route path='/edit-department/:id' element = { <Department />}></Route>
-        </Routes>
-        <Footer />
+
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <div className="flex-grow">
+            <Routes>
+              {/* // http://localhost:3000 */}
+              <Route path="/" element={<EmployeeList />}></Route>
+              {/* // http://localhost:3000/employees */}
+              <Route path='/employees' element={<EmployeeList />}></Route>
+              {/* // http://localhost:3000/add-employee */}
+              <Route path='/add-employee' element={<Employee />}></Route>
+              {/* // http://localhost:3000/edit-employee/1 */}
+              <Route path='/edit-employee/:id' element={<Employee />}></Route>
+              {/* // http://localhost:3000/departments */}
+              <Route path='/departments' element={<DepartmentList />}></Route>
+              {/* // http://localhost:3000/add-department */}
+              <Route path='/add-department' element={<Department />}></Route>
+              {/* // http://localhost:3000/edit-department */}
+              <Route path='/edit-department/:id' element={<Department />}></Route>
+            </Routes>
+          </div>
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
   );
